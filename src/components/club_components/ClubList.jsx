bdcd67223653a1206e4f5dd70bd390f949584fdc
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-19 17:31:03 -0500

    create components

---
+++ src/components/club_components/ClubList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClubList = () => {
+  return (
+    <div>ClubList</div>
+  )
+}
+
+export default ClubList
---
+++ src/components/club_components/CreateClub.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateClub = () => {
+  return (
+    <div>CreateClub</div>
+  )
+}
+
+export default CreateClub
