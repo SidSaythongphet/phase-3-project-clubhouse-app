ba441d68feab09a2d87df284a51c9318ef5400f6
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-20 00:42:27 -0500

    add log in capabilities

--- src/components/session_components/Login.jsx
+++ src/components/session_components/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+const Login = ({ loginUser, users }) => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    const navigate = useNavigate()
+    
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (e) => {
+      e.preventDefault()
+      const user = users.find(user => user.username.toLowerCase() === formData.username.toLowerCase())
+      if (user && user.password === formData.password) {
+        loginUser(user)
+        navigate('/')
+      } else {
+        alert('Invalid credentials')
+      }
+    }
+
+    return (
+        <Paper
+            component="form"
+            noValidate
+            autoComplete="off"
+            >
+            <Stack>
+                <Typography>Login</Typography>
+                <TextField
+                    required
+                    id="outlined-username-required"
+                    label="Username"
+                    name="username"
+                    value={ formData.username }
+                    variant='filled'
+                    onChange={ handleChange }
+                    />
+                <TextField
+                    required
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={ formData.password }
+                    variant='filled'
+                    onChange={ handleChange }
+                    />
+                <Button onClick={ handleClick } >
+                    Login
+                </Button>
+            </Stack>
+        </Paper>
+    )
 }
 
 export default Login
