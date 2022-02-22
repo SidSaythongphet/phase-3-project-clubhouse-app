2620e784f06e45776a729f1334c84012661b066a
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-22 12:34:39 -0500

    style user home page

--- src/components/session_components/UserHomePage.jsx
+++ src/components/session_components/UserHomePage.jsx
@@ -1,33 +1,78 @@
-import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { Box, Container, Grid, Paper, Typography } from '@mui/material';
 import { Avatar } from '@mui/material';
+import StyledButton from '../../styled_components/StyledButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ClubList from '../club_components/ClubList';
 
 
-const UserHomePage = ({ loggedIn, currentUser }) => {
-  
-  const homeLoggedIn = () => {
-    const { first_name, last_name, username, clubs } = currentUser
-
+const UserHomePage = ({ currentUser, currentClub, usersClubs }) => {
+    const { first_name, last_name, username } = currentUser
+    
     return (
-      <Paper>
-        <Avatar
-          alt={ first_name + last_name }
-          sx={{ width: 100, height: 100 }}
-        >
-          { first_name[0] + last_name[0]}</Avatar>
-        <Typography>Full Name: { first_name } { last_name }</Typography>
-        <Typography>Userame: { username }</Typography>
-        <Typography>My Clubs</Typography>
-        { clubs.map(club => <Typography key={ club.id }>{ club.club_title }</Typography>) }
-      </Paper>
+      <Container style={{padding:'0 24px'}}>
+        {
+          !currentUser 
+          ?
+          <Typography align='center'>Loading profile...</Typography>
+          :
+          <Grid container spacing={3} display='flex' justifyContent='center'>
+            <Grid item xs={10} sm={7} md={5} lg={4} style={{padding: '10px'}}>
+              <Typography gutterBottom variant='h5' style={{ fontWeight: 'bold', marginLeft: '20px' }}>My Account</Typography>
+              <Paper
+                elevation={3}
+                style={{
+                  height: '320px',
+                  minWidth: '320px',
+                  padding: '20px 5px 10px',
+                  borderRadius: '25px', 
+                  background: 'linear-gradient(200deg, rgba(2,0,36,.9) 20%, rgba(9,79,121,0.75)65%, rgba(0,212,255,.9) 85%)'
+                }}
+              >
+                <Grid container spacing={3} justifyContent='center'>
+                  <Grid item xs={12}>
+                    <Avatar
+                      alt={ first_name + last_name }
+                      sx={{ width: 100, height: 100 }}
+                      style={{ margin: 'auto' }}
+                      >
+                      { first_name[0] + last_name[0] } 
+                    </Avatar>
+                  </Grid>
+                  <Grid item>
+                    <Typography align='center' variant='h6'>Full Name:</Typography>
+                    <Typography align='center' style={{ color: '#fefefe', backgroundColor: 'rgba(255, 255, 255, 0.10)', fontWeight: 'bold', padding: '0 15px', minInlineSize: '250px', borderRadius: '25px' }} gutterBottom>{ first_name } { last_name }</Typography>
+                    <Typography align='center' variant='h6'>Userame:</Typography> 
+                    <Typography align='center' style={{ color: '#fefefe', backgroundColor: 'rgba(255, 255, 255, 0.10)', fontWeight: 'bold', padding: '0 15px', minInlineSize: '250px', borderRadius: '25px' }}>{ username }</Typography>
+                  </Grid>
+                  <Grid item >
+                    <StyledButton icon={ <SettingsIcon/> } text='Settings'/>
+                    <StyledButton icon={ <DeleteIcon/> } color='secondary' text='Delete Account'/>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+            <Grid item xs={10} sm={7} md={7} lg={8} style={{padding: '10px'}}>
+              <Typography gutterBottom variant='h5' style={{ fontWeight: 'bold', marginLeft: '20px' }}>My Clubs</Typography>
+              <Paper
+                elevation={3}
+                style={{
+                  minHeight: '500px',
+                  minWidth: '320px',
+                  padding: '20px 5px 10px',
+                  borderRadius: '25px',
+                  background: 'linear-gradient(200deg, rgba(2,0,36,.9) 20%, rgba(9,79,121,0.75)65%, rgba(0,212,255,.9) 85%)'
+                }}
+              >
+                <ClubList clubs={ usersClubs } currentClub={ currentClub }/>
+              </Paper>
+            </Grid>
+          </Grid>
+        }
+      </Container>
     )
-  }
 
-  return (
-    <>
-      { loggedIn ? homeLoggedIn() : <h1>Not Logged In</h1> }
-    </>
-  )
 }
 
 export default UserHomePage
