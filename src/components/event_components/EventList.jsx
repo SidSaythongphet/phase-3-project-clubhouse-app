3b6de96e99fd9e602e25665ae7e1ec49f4fc83e8
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-24 02:03:07 -0500

    expand event component

--- src/components/event_components/EventList.jsx
+++ src/components/event_components/EventList.jsx
@@ -1,23 +1,37 @@
-import { Card, Typography } from '@mui/material'
+import { Typography, Divider, Box, Container, Grid } from '@mui/material'
 import React from 'react'
-import StyledButton from '../../styled_components/StyledButton'
 import CreateEvent from './CreateEvent'
 import EventItem from './EventItem'
-import { format, compareDesc } from 'date-fns'
 
 
 const EventList = ({ events, club_id, onAddEvent }) => {
-  const today = format(new Date(), 'yyyy-MM-dd')
-  const dates = events.map(event => event.event_date.split('T')[0])
   const sorted_events = [...events].sort((a,b) => new Date(a.event_date) - new Date(b.event_date)).reverse()
-  const renderEvents = sorted_events.map(event => <EventItem key={ event.id } event={ event } />)
+  const renderEvents = sorted_events.map(event => {
+    return (
+      <Box
+        key={ event.id }
+        style={{
+          margin: '5px 0',
+          borderRadius: '15px', 
+          background: 'linear-gradient(200deg, rgba(2,0,36,1) 0%, rgba(9,79,121,1) 50%, rgba(58,124,138,1) 100%)'
+        }}
+      >
+        <EventItem event={ event } />
+        <Divider variant="middle" />
+      </Box>
+    )
+  })
 
   return (
-    <div>
-      <Typography>EventList</Typography>
-      { renderEvents }
-      <CreateEvent club_id={ club_id } onAddEvent={ onAddEvent }/>
-    </div>
+    <>
+      <Box sx={{ width: '100%', bgcolor: 'background.paper', borderRadius:'5px' }} style={{paddingBottom: '20px', marginBottom: '50px'}}>
+        <Container>
+          <Typography>EventList</Typography>
+          <CreateEvent club_id={ club_id } onAddEvent={ onAddEvent }/>
+          { renderEvents }
+        </Container>
+      </Box>
+    </>
   )
 }
 
